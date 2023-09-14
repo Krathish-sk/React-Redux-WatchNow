416c1a61d3d7642d7a5c132dd6f6cf7977c76682
Krathish K <krathishs.k123@gmail.com>
2023-09-14 17:27:15 +0530

    Display Like feature in MovieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AiTwotoneHeart } from "react-icons/ai";
 import "./MovieCard.scss";
 
 export default function MovieCard({ data }) {
+  const { likedMovies } = useSelector((state) => state.user);
+
+  const likedMovie = likedMovies.filter(
+    (movie) => movie.imdbID === data.imdbID
+  );
+  console.log(
+    "🚀 ~ file: MovieCard.jsx:15 ~ MovieCard ~ likedMovie:",
+    likedMovie
+  );
+
   return (
     <div className="card-item">
       <Link to={`/movie/${data.imdbID}`}>
@@ -14,6 +26,7 @@ export default function MovieCard({ data }) {
             <div className="card-info">
               <h4>{data.Title}</h4>
               <p>{data.Year}</p>
+              {likedMovie.length !== 0 && <AiTwotoneHeart className="liked" />}
             </div>
           </div>
         </div>
